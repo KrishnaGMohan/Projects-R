3afdfa9f38792ef1674c2e7f8ddfc81f99a92539
KrishnaGMohan <krishna.gandhimohan@hotmail.ca>
2016-07-29 15:59:09 -0400

    Commit 20160729

--- 01.R-Basics/01.R-Basics/01.a.Arithmetic_and_DataTypes.R
+++ 01.R-Basics/01.R-Basics/01.a.Arithmetic_and_DataTypes.R
@@ -69,4 +69,28 @@ rm(z)
 # Delete all objects
 rm(list = ls())
 
+#------------------------------------
+# Sequences of Numbers
+#------------------------------------
+
+1:5
+3:-2
+seq(2, 9, by = 3)
+seq(2, 4, by = 0.5)
+seq(4, 8, length = 8)
+
+x <- c(1, 3, 5, 7, 9, 11)
+1:length(x)
+
+x <- c(1, 3, 5, 7, 9, 11)
+seq(along.with = x)
+
+x <- c(1, 3, 5, 7, 9, 11)
+seq_along(x)
+
+rep(0, 5)
+rep(c(1, 2, 3), times = 3)
+rep(c(0, 1, 2), each = 3)
+rep(c(1, 2), c(2, 3))
+rep(c(1:3), length.out = 10)
 
