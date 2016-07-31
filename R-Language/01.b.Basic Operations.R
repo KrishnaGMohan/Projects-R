03eca5f62327c98156b2b2e3fc643e072b548efa
KrishnaGMohan <krishna.gandhimohan@hotmail.ca>
2016-07-31 10:53:01 -0400

    20160731 001

--- R-Language/01.b.Basic Operations.R
+++ R-Language/01.b.Basic Operations.R
@@ -63,3 +63,52 @@ v
 v <- c("Every cloud has a silver lining")
 substring(v, 19) <- c("platinum")
 v
+
+s1 = c("waste")
+s2 = c("not")
+s3 = c("want")
+
+paste(s1, s2, s3, s2)
+paste(s1, s2, s3, s2, sep = "")
+paste(s1, s2, s3, s2, sep = "-")
+paste(c(s1, s3), c(s2, s3), sep = "-")
+paste(c(s1, s3), c(s2, s3), sep = "-", collapse = ", ")
+
+month.abb = c("Jan", "Feb", "Mar", "Apr", "May", "Jun")
+month.abb
+nth <- paste0(1:6, c("st", "nd", "rd", rep("th", 3)))
+nth
+paste(month.abb, nth, sep = ": ", collapse = "; ")
+
+
+today <- Sys.Date()
+today
+class(today)
+
+now <- Sys.time()
+now
+class(now)
+
+
+xdate <- as.Date("1970-10-31")
+xdate
+class(xdate)
+
+
+str1 <- "Aug 22, '07"str2 <- "1992-11-15"str3 <- "30/January/89"date1 <- as.Date(str1, format = "%b %d, '%y")date2 <- as.Date(str2)date3 <- as.Date(str3, format = "%d/%B/%y")format(date1, "%A")format(date2, "%B %dth, %Y, %a")format(date3, "%b %Y")s1 <- c("1996-05-15 09:15:25")
+s2 <- c("May 23, '96 hours:23 minutes:01 seconds:45")time01 <- as.POSIXct(s1)time02 <- as.POSIXct(s2, format = "%B %d, '%y hours:%H minutes:%M seconds:%S")format(time01, "%H.%M")
+format(time02, "%I:%M %p")
+
+#----------------------------------------
+
+date01 <- as.Date("2015-10-31") 
+date02 <- as.Date("2015-06-12")
+
+date01 - date02
+date02 - date01
+
+date01 + 45
+date02 - 10
+
+
+
